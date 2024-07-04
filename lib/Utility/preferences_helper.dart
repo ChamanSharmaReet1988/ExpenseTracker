b65e46cb2f6c618ee8623a6a5d8ccfaab9df8740
Sakshi choudhary <sakshichoudhary565@gmail.com>
2024-07-04 18:45:32 +0530

    Merge branch 'main' of https://github.com/ChamanSharmaReet1988/ExpenseTracker

---
+++ lib/Utility/preferences_helper.dart
@@ -0,0 +1,39 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class PreferencesHelper {
+  static final PreferencesHelper _instance = PreferencesHelper._internal();
+  late SharedPreferences _prefs;
+
+  factory PreferencesHelper() {
+    return _instance;
+  }
+
+  PreferencesHelper._internal();
+
+  Future<void> init() async {
+    _prefs = await SharedPreferences.getInstance();
+  }
+
+  // Getter and Setter for username
+  String? get username => _prefs.getString('username');
+  set username(String? value) {
+    _prefs.setString('username', value ?? '');
+  }
+
+  // Getter and Setter for age
+  int? get age => _prefs.getInt('age');
+  set age(int? value) {
+    _prefs.setInt('age', value ?? 0);
+  }
+
+  // Getter and Setter for isLoggedIn
+  bool? get isLoggedIn => _prefs.getBool('isLoggedIn');
+  set isLoggedIn(bool? value) {
+    _prefs.setBool('isLoggedIn', value ?? false);
+  }
+
+  // Method to remove all preferences
+  Future<void> clear() async {
+    await _prefs.clear();
+  }
+}
