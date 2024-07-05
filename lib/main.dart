88c3a58a65b79c68ac028bef2b2ee1d8f6a1a05e
Sakshi choudhary <sakshichoudhary565@gmail.com>
2024-07-05 13:30:44 +0530

    mplemented a feature where the user only needs to log in once and is automatically taken to the home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,53 @@
+import 'package:expense_tracker/OtpScreen/welcome_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:expense_tracker/LoginScreen/login_view.dart';
-import 'package:expense_tracker/LoginScreen/login_view.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 // ignore: file_names
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
+// ignore: must_be_immutable
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+    
+  Future<bool> checkLogin() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  return prefs.getBool('isLoggedIn') ?? false;
+}
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+   return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: FutureBuilder<bool>(
+        future: checkLogin(),
+        builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          } else if (snapshot.hasError) {
+            return Scaffold(
+              body: Center(
+                child: Text('Error: ${snapshot.error}'),
+              ),
+            );
+          } else {
+            bool isLoggedIn = snapshot.data ?? false;
+            return isLoggedIn ? WelcomeScreen() : LoginPage();
+          }
+        },
+      ),
     );
+
   }
 }
